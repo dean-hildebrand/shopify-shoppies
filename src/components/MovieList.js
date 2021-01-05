4c0383149e938106530ea0d1f209885814489535
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-01-04 23:38:08 -0500

    Row of films, hover effect

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../css/MovieList.css";
 
 function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map((movie, index) => (
-        <div>
+        <div className='movie'>
           <h2>{movie.Title}</h2>
           <img src={movie.Poster} alt={movie.Title} />
         </div>
