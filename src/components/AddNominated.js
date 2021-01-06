4aa887748e1869c4f5b3a725cdb84d6954b8247e
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-01-06 12:52:57 -0500

    Finished styling

--- src/components/AddNominated.js
+++ src/components/AddNominated.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/AddNominated.css";
 import AddIcon from "@material-ui/icons/Add";
 import { Button } from "@material-ui/core";
 
 function AddNominated() {
+
   return (
     <div className="add-btn">
       <span className="add-nominated">Nominate</span>
